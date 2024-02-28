f6387ae2ef4cbf526103574e937be0f5a950fa29
Omnath Mandal <145841395+htanmo@users.noreply.github.com>
2024-02-29 00:22:25 +0530

    <>< config updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,13 @@ end
 # Rust Cargo path
 set PATH $PATH ~/.cargo/bin;
 
+# Go Path
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
 set -U fish_greeting ""
+
+set -gx TERM xterm-256color
 
 set -U fish_features stderr-nocaret qmark-noglob
 
--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -2,13 +2,16 @@ function fish_mode_prompt --description 'Displays the current mode'
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color --bold red
+                set_color --bold green
                 echo 🅽
             case insert
-                set_color --bold green
+                set_color --bold cyan
                 echo 🅸
-            case replace-one
-                set_color --bold green
+            case replace_one
+                set_color --bold yellow
+                echo 🆁
+            case replace
+                set_color --bold yellow
                 echo 🆁
             case visual
                 set_color --bold brmagenta
