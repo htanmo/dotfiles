8e511facba3618989518727ad2912af79cb1d324
Omnath Mandal <145841395+htanmo@users.noreply.github.com>
2024-03-26 00:00:28 +0530

    plugin: nvim
    added plugin hop.nvim and nvim-surround

--- .config/nvim/lua/htanmo/plugins.lua
+++ .config/nvim/lua/htanmo/plugins.lua
@@ -78,4 +78,19 @@ require("lazy").setup({
     -- Oil 
     { 'stevearc/oil.nvim', },
 
+    -- Surround
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
+    -- hop.nvim
+    { 'phaazon/hop.nvim', },
+
 }, {})
