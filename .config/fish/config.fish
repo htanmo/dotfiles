82f48844f455024b76ebca13c3bebd12773a6938
Omnath Mandal <145841395+htanmo@users.noreply.github.com>
2024-08-15 00:13:19 +0530

    update <>< config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,21 @@
 if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
 
-# Rust Cargo path
-set PATH $PATH ~/.cargo/bin;
-
-# Go Path
-set -g GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
-
 set -U fish_greeting ""
-
 set -gx TERM xterm-256color
 
-set -U fish_features stderr-nocaret qmark-noglob
+# .local/bin path
+set -gx PATH $PATH $HOME/.local/bin
 
-# Zoxide
+# rust
+set -gx PATH $PATH $HOME/.cargo/bin
+
+# golang
+set -gx GOROOT /usr/local/go
+set -gx PATH $PATH $GOROOT/bin
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH $GOPATH/bin
+
+# zoxide
 zoxide init fish | source
